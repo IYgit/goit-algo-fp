38d30912c8bb417c4da47f5267c27d1504167841
IYgit <novemb75@gmail.com>
2024-05-25 22:37:53 +0300

    task 5

--- task_4.py
+++ task_4.py
@@ -25,15 +25,16 @@ def add_edges(graph, node, pos, x=0, y=0, layer=1):
             r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
     return graph
 
-def draw_tree(tree_root):
+def draw_tree(tree_root, title):
     tree = nx.DiGraph()
     pos = {tree_root.id: (0, 0)}
     tree = add_edges(tree, tree_root, pos)
 
     colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
 
     plt.figure(figsize=(8, 5))
+    plt.title(title)
     nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
     plt.show()
 
@@ -67,4 +68,5 @@ values = [10, 15, 20, 17, 25]
 heap_root = build_heap(values)
 
 # Відображення бінарної купи
-draw_tree(heap_root)
+if __name__ == "__main__":
+    draw_tree(heap_root, "Відображення бінарної купи")
